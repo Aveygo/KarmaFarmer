9d2cd4bb852f07907bddb380a6680836038340c1
CodingCoda <48315552+CodingCoda@users.noreply.github.com>
2021-04-15 10:17:34 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
   packages = ['KarmaFarmer'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='wtfpl',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Joins and participates in r/KarmaStreet to upvote other users thus inf. karma',   # Give a short description about your library
+  description = 'Joins and participates in r/KarmaStreet to upvote other users thus creating an upvote "pool". Contains anti-spam solution.',   # Give a short description about your library
   author = 'Dmitri Shevchenko',                   # Type in your name
   author_email = 'dmitri.shevchenko.au@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/CodingCoda/KarmaFarmer',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/CodingCoda/KarmaFarmer/archive/refs/tags/v0.1-alpha.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/CodingCoda/KarmaFarmer/archive/refs/tags/0.1.tar.gz',    # I explain this later on
   keywords = ['reddit', 'karma', 'bot'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
             'datetime',
